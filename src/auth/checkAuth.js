683c55aa2cc36da6753c194eacf160478250f606
AdminJohnk <truongtantai07042002@gmail.com>
2023-08-27 14:14:08 +0700

    ApiKey and Permissions

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const APIKeyModel = require('../models/apiKey.model');
+
+const HEADER = {
+  API_KEY: 'x-api-key',
+  AUTHORIZATION: 'authorization'
+};
+
+const checkApiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    if (!key) {
+      return res.status(403).json({
+        message: 'Forbidden Error'
+      });
+    }
+    // check objKey
+    const objKey = await APIKeyModel.findByID(key);
+    if (!objKey) {
+      return res.status(403).json({
+        message: 'Forbidden Error'
+      });
+    }
+
+    req.objKey = objKey;
+    return next();
+  } catch (error) {
+    console.log(error.message);
+    return;
+  }
+};
+
+const checkPermission = permission => {
+  return (req, res, next) => {
+    if (!req.objKey.permissions) {
+      return res.status(403).json({
+        message: 'Permission denied'
+      });
+    }
+
+    const validPermission = req.objKey.permissions.includes(permission);
+    if (!validPermission) {
+      return res.status(403).json({
+        message: 'Permission denied'
+      });
+    }
+    return next();
+  };
+};
+
+module.exports = {
+  checkApiKey,
+  checkPermission
+};
--- src/routes/root.router.js
+++ src/routes/root.router.js
@@ -3,6 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const authRouter = require('./auth.router');
+const { checkApiKey,checkPermission } = require('../auth/checkAuth');
+
+// check apiKey
+router.use(checkApiKey);
+
+// check permission
+router.use(checkPermission('0000'));
 
 router.use(authRouter);
 
