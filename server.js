208d2f59109abeaeb55f634c1b61db7afa24c011
ZLoganZ <ln26805@gmail.com>
2023-12-19 09:49:08 +0700

    Update PORT variable in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = process.env.DEV_APP_PORT || 3056;
+const PORT = process.env.PORT || 3056;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
