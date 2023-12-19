a3a1180b060789418bcd314781d5d8f3354b7a62
ZLoganZ <ln26805@gmail.com>
2023-12-19 17:19:16 +0700

    Update MongoDB connection string

--- src/database/init.mongodb.js
+++ src/database/init.mongodb.js
@@ -20,7 +20,7 @@ class Database {
       // const connectString = `mongodb://${host}:${port}/${name}`;
 
       const connectString =
-        'mongodb+srv://socialnetwork:IsBSBM6L1CFiiQWL@socialcluster.i599n1a.mongodb.net/SocialProDEV';
+        'mongodb+srv://socialnetwork:IsBSBM6L1CFiiQWL@socialcluster.i599n1a.mongodb.net/SocialProTest';
 
       mongoose
         .connect(connectString, {
