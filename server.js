22db69c0d939ebf817dff61012f8c41361ca8bf8
ZLoganZ <ln26805@gmail.com>
2024-04-13 10:40:27 +0700

    Update RabbitMQ initialization and AWS SDK versions

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ require('./src/database/init.mongodb');
 SenderMailServer();
 
 // init rabbitmq
-// const RabbitInit = require('./src/database/init.rabbit');
-// RabbitInit.connectToRabbitMQ().then(({ channel, connection }) => {
-//   global.__channel = channel;
-//   global.__connection = connection;
-// });
+const RabbitInit = require('./src/database/init.rabbit');
+RabbitInit.connectToRabbitMQ().then(({ channel, connection }) => {
+  console.log('Connected to RabbitMQ');
+  global.__channel = channel;
+  global.__connection = connection;
+});
 
 // init redis
 // const RedisInit = require('./database/init.redis');
